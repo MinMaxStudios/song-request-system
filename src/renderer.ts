65d36715dbf2bb6d5fb42949a56e48488db9b716
ToastedToast <hey@toasted.dev>
2024-06-06 17:29:38 +0800

    feat: youtube iframe on page

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,18 @@
+/// <reference types="youtube" />
+
 import "./index.css";
 
 console.log(
   '👋 This message is being logged by "renderer.ts", included via Vite',
 );
+
+const player = new YT.Player("player", {
+  height: "390",
+  width: "640",
+  videoId: "dQw4w9WgXcQ",
+  events: {
+    onReady: () => {
+      player.playVideo();
+    },
+  },
+});
